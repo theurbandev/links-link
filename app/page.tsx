9b8c94cae8f1cde8e703e8db6c25e47a6e840451
mattjoe <matthew.j.joe@gmail.com>
2023-11-07 22:19:59 -0500

    excaping character and making background black

--- app/page.tsx
+++ app/page.tsx
@@ -23,13 +23,13 @@ export default function Home() {
     return (
         <>
             <section>
-                <h1 className="text-6xl font-extrabold px-10 pt-10">MATT</h1>
-                <h3 className="text-xs font-semibold px-10 py-1">@THEURBANDEV</h3>
+                <h1 className="text-6xl font-extrabold px-10 pt-10 text-white">MATT</h1>
+                <h3 className="text-xs font-semibold px-10 py-1 text-white">@THEURBANDEV</h3>
             </section>
 
             <main className="flex flex-col min-h-screen items-center">
                 <div className="grid gap-4 grid-cols-1 w-full p-24 max-w-screen-sm justify-center">
-                    <p className="text-center pb-10">everything you're looking for.</p>
+                    <p className="text-center pb-10">everything you{"'"}re looking for.</p>
                     {Object.keys(links).map((key: any) => (
                         <Link key={key} url={links[key].url} logo={links[key].logo} text={key} />
                     ))}
